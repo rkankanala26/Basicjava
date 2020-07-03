2df3f4f7d42306fa3492f6f4fcaf1e1fab9c7243
rkankanala26 <rkankanala26@gmail.com>
2020-07-03 16:27:26 -0700

    simple math hw

---
+++ HelloWorld/src/MySimpleMath.java
@@ -0,0 +1,23 @@
+
+public class MySimpleMath {
+
+	public static void main(String[] args) {
+		int x;
+		x = 20;
+		
+		int y = 30;
+		
+		int Result1;
+		int Result2;
+	Result1 = x + y;
+	Result2 = x * y;
+	
+	System.out.println("The result is " + Result1);
+	System.out.println("The multipication is " + Result2);
+		
+				
+       
+	}  		
+	
+     
+}
