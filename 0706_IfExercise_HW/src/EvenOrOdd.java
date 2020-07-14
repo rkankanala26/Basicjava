a33bf4e65228e7afeed0efceec39d0779db875fa
rkankanala26 <rkankanala26@gmail.com>
2020-07-13 17:53:22 -0700

    0706_IfExercise Homework

---
+++ 0706_IfExercise_HW/src/EvenOrOdd.java
@@ -0,0 +1,21 @@
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		
+		
+		// Even or Odd number
+				int x = 13;
+				
+				if (x % 2 == 0){
+				 System.out.println (x + " is even");
+				 
+				}else {
+				   System.out.println(x + " is odd");
+			
+
+	}
+
+}
+
+}
---
+++ 0706_IfExercise_HW/src/If_Block.java
@@ -0,0 +1,30 @@
+
+public class If_Block {
+
+	public static void main(String[] args) {
+	
+		
+
+	int testscore = 100;
+		
+		if(testscore >=90){
+		
+			System.out.println("The grade is  A");
+			
+			
+		}else if(testscore>=80){
+			System.out.println("The Grade is  B");
+
+	}else if (testscore >=60){
+		System.out.println("The Grade is C");
+		
+	}else {
+		System.out.println("The Grade is D");
+	}
+		}
+
+
+		
+	}
+
+
