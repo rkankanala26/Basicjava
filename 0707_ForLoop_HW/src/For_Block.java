f8bdf9165a549164077640ab1bb942bdd5e6595a
rkankanala26 <rkankanala26@gmail.com>
2020-07-13 18:36:38 -0700

    ForLoop Homework

---
+++ 0707_ForLoop_HW/src/For_Block.java
@@ -0,0 +1,37 @@
+
+public class For_Block {
+
+	public static void main(String[] args) {
+		// For Loop for increment 
+		// Starting number: 10
+		// every step : increment by 10
+		// End : stop at 100;
+		
+				int x =10;
+						 
+			 for( ;  x <=100 ;  x = x+10){
+					System.out.println(x);
+			
+				}
+		
+			// For Loop for decrement
+			// Starting number :100
+			// every step: decrement by 10
+			// End : stop 10;
+					
+		/*	
+			 int x = 100;
+				
+				for ( ; x>=10; x = x-10){
+					System.out.println (x);
+					
+			*/		
+					
+					
+				}				
+				
+				
+			}
+
+
+
---
+++ 0707_ForLoop_HW/src/MathTable.java
@@ -0,0 +1,20 @@
+
+public class MathTable {
+
+	public static void main(String[] args) {
+		
+		
+		// printing Math 6th table
+		
+		for(int x = 0; x<=10 ; x++) {
+			
+			int tableNumber = 6;	
+			
+			System.out.println(tableNumber + " * " + x + " = " + tableNumber*x);
+			
+	}
+
+}
+	
+}
+
