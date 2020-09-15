37cd581fa40fa3dc9e3fe95a71cece40d070efcf
rkankanala26 <rkankanala26@gmail.com>
2020-09-15 16:26:42 -0700

    Load Initial Project

---
+++ MyFP/src/Student.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Student {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter your grade");
+
+		int Grade = scanner.nextInt();
+
+		if (Grade >= 1 && Grade <= 5) {
+			System.out.println("Elementary School");
+		}
+
+		else if (Grade >= 6 && Grade <= 8) {
+			System.out.println("Middle School");
+
+		} else if (Grade >= 9 && Grade <= 12) {
+			System.out.println("High School");
+		}
+
+		else {
+			System.out.println("College");
+		}
+		
+		scanner.close();
+
+	}
+
+}
