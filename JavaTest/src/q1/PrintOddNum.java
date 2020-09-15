631a2f07d5656d4342069d8ba1e2e1920a78d72b
rkankanala26 <rkankanala26@gmail.com>
2020-09-15 16:32:47 -0700

    Restage q1

--- JavaTest/src/q1/PrintOddNum.java
+++ JavaTest/src/q1/PrintOddNum.java
@@ -1,9 +1,20 @@
 package q1;
 
-public class MyClass {
+public class PrintOddNum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Print all odd numbers between 1-100");
+		for (int i=0; i<=100; i++) {
+			
+			if (i%2==1) {
+				System.out.print(i);
+				System.out.printf(",");
+//				System.out.printf(i,",");
+			}
+			
+		}
+		
 
 	}
 
