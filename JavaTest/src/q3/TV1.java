03fe35176d2dc02dbba954f45b04c74411e7bbc4
rkankanala26 <rkankanala26@gmail.com>
2020-09-15 17:39:28 -0700

    Upload new version

--- JavaTest/src/q3/TV1.java
+++ JavaTest/src/q3/TV1.java
@@ -1,6 +1,6 @@
 package q3;
 
-public class TV {
+public class TV1 {
 	private String color;
 	private int size;
 	private String model;
@@ -8,29 +8,29 @@ public class TV {
 	private double price;
 
 	//TV has color, size, model, make, price and some more
-	public TV (String color) {
+	public TV1 (String color) {
 		this.color=color;
 	}
 	
-	public TV (int size, String model) {
+	public TV1 (int size, String model) {
 		this.size = size;
 		this.model= model;
 	}
 	
-	public TV() {
+	public TV1() {
 		this.make="Samsung";
 		this.price=1200;
 	}
 	public static void main(String[] args) {
 		
-		TV objTV1 = new TV("Blue");
+		TV1 objTV1 = new TV1("Blue");
 		System.out.println("My TV Color is " + objTV1.color);
 		
-		TV objTV2 = new TV(46,"LED TV");
+		TV1 objTV2 = new TV1(46,"LED TV");
 		System.out.println("      Size is " + objTV2.size +" inches ");
 		System.out.println("      Model is "+objTV2.model);
 		
-		TV objTV3 = new TV();
+		TV1 objTV3 = new TV1();
 		System.out.println("      Make is "+objTV3.make );
 		System.out.println("      Price is: $"+ objTV3.price);
 
